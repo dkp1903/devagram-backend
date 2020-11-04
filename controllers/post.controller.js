d922a648fa36ed5450a131f7654059cb9295239a
ayushpaharia <ayush.paharia.18@gmail.com>
2020-11-04 22:08:38 +0530

    CRUD post routes added

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,6 +2,7 @@ const Post = require("../models/post_schema");
 
 const { validationResult } = require("express-validator");
 const showError = require("../config/showError");
+const e = require("express");
 
 const addPost = async (req, res) => {
   const errors = validationResult(req);
@@ -34,6 +35,59 @@ const addPost = async (req, res) => {
   }
 };
 
+const getAllPosts = async (req, res) => {
+  const { id } = req.user;
+  try {
+    const posts = await Post.find({ user: id })
+      .populate("user", "_id")
+      .sort("createdAt");
+
+    return res.status(200).json({ posts });
+  } catch (error) {
+    showError(error);
+  }
+};
+
+const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id).populate("user", "_id");
+    if (!post) return res.status(404).json("Post not found");
+    return res.status(200).json({ post });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const postDelete = async (req, res) => {
+  try {
+    const deletedPost = await Post.findByIdAndDelete(req.params.id);
+    if (deletedPost.user.id === req.user.id) {
+      if (!deletedPost) return res.status(404).json("Post not found");
+      return res.status(200).json("Post Deleted!");
+    } else return res.status(401).json("Post access denied!");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const postEdit = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (deletedPost.user.id === req.user.id) {
+      if (!post) return res.status(404).json("Post not found");
+      if (content) post.content = req.body.content;
+    } else return res.status(401).json("Post access denied!");
+
+    return res.status(200).json("Post Updated");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 module.exports = {
   addPost,
+  getAllPosts,
+  getPost,
+  postDelete,
+  postEdit,
 };
--- router/post.js
+++ router/post.js
@@ -4,9 +4,15 @@ const { check } = require("express-validator");
 
 //middleware
 const requireLogin = require("../middlewares/requireLogin");
+const checkOriginalUser = require("../middlewares/checkOriginalUser");
 
 //controllers
-const { addPost } = require("../controllers/post.controller");
+const {
+  addPost,
+  getAllPosts,
+  getPost,
+  postDelete,
+} = require("../controllers/post.controller");
 
 /**
  * route : POST /api/post
@@ -22,5 +28,33 @@ router.post(
   ],
   addPost
 );
+
+/**
+ * route : GET /api/post
+ * access : Public
+ * desc: GET posts of all users
+ */
+router.get("/", getAllPosts);
+
+/**
+ * route : GET /api/post
+ * access : Private
+ * desc: GET single post
+ */
+router.get("/:id", requireLogin, getPost);
+
+/**
+ * route : DELETE /api/post
+ * access : Private
+ * desc: DELETE post
+ */
+router.delete("/:id", requireLogin, postDelete);
+
+/**
+ * route : PUT /api/post
+ * access : Private
+ * desc: EDIT post
+ */
+router.put("/:id", requireLogin, postDelete);
 
 module.exports = router;

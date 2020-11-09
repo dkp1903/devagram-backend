58c22df545719e4539beb3b5af3df91e86eb01ff
ayushpaharia <ayush.paharia.18@gmail.com>
2020-11-10 04:17:18 +0530

    issue #33, #23, #32 resolved

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -134,6 +134,31 @@ const postUnlike = (req, res) => {
   });
 };
 
+const postComment = (req, res) => {
+  const comment = {
+    text: req.body.text,
+    user: req.user._id,
+  };
+  Post.findByIdAndUpdate(
+    req.params.id,
+    {
+      $push: { comments: comment },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("comments.user", "_id name")
+    .populate("user", "_id name")
+    .exec((err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      } else {
+        res.status(200).json({ post: result, message: "comment added!" });
+      }
+    });
+};
+
 module.exports = {
   addPost,
   getAllPosts,
@@ -144,4 +169,5 @@ module.exports = {
   getAllLikes,
   postLike,
   postUnlike,
+  postComment,
 };

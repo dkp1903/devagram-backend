faa99d11d12fb758a17d3f8506f5a1f5a42e2f4f
Darshil Parikh <58654113+201901407@users.noreply.github.com>
2020-11-16 19:57:00 +0530

    Update and rename jobs_schema.js to job_schema.js

---
+++ models/job_schema.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const JobSchema = new mongoose.Schema({
+  job_title: {
+    type: String,
+    required: true,
+  },
+  job_type: {
+    type: String,
+    required: true,
+  },
+  job_description: {
+    type: String,
+    required: true,
+  },
+  job_start: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: String,
+    required: true,
+    default: 0,
+  },
+  job_applicants: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      contact_details: {
+        type: String,
+      },
+    },
+  ],
+});
+
+mongoose.model("Job", JobSchema);

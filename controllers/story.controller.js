08cbdcf86c7baf6a443ef639146932d6ca7160a8
MananDesai54 <manan5401desai@gmail.com>
2021-01-04 15:59:48 +0530

    story done

--- controllers/story.controller.js
+++ controllers/story.controller.js
@@ -1,5 +1,5 @@
 const Story = require("../models/story_schema");
-const User = require("../models/user_schema");
+const cloudinary = require("../utils/cloudinary");
 
 const showError = require("../utils/showError");
 const { validationResult } = require("express-validator");
@@ -12,11 +12,12 @@ const uploadStory = async (req, res) => {
     });
   }
 
-  const { imageUrl, content } = req.body;
+  const { content } = req.body;
   try {
+    const imageResult = await cloudinary.uploader.upload(req.file.path);
     const story = new Story({
       user: req.user.id,
-      imageUrl,
+      imageUrl: imageResult.url,
     });
     if (content) story.content = content;
     await story.save();
